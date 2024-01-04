b3f58f2d9e113cc18875c10f603dd4e067d1c3b6
MrClassicT <tristan.cuvelier@student.hogent.be>
2024-01-04 16:54:32 +0100

    small todo update
    + Source that still could be useful.

--- app/src/main/java/com/android/dailycat/TODOS.kt
+++ app/src/main/java/com/android/dailycat/TODOS.kt
@@ -5,6 +5,7 @@ package com.android.dailycat
 
 // Hier was ik gebleven :)
 // TODO - DTO? Zien om catpost te generaten adhv api data, zo te plaatsen in lokale catpost en deze weer te geven.
+// SRC: https://dev.to/arkilis/download-image-from-url-in-kotlin-4fc6
 
 // TODO - COIL for images? -> https://github.com/coil-kt/coil#jetpack-compose
 // TODO - add contentDescription for visual elements. -> If element is simply for visual decoration, set it to Null.
