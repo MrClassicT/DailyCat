e37b4e378cda761b0dc9bec5c1ff5248f628a582
MrClassicT <tristan.cuvelier@student.hogent.be>
2024-01-06 22:07:47 +0100

    build.gradle.kts both app and project updated.
    Included room dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -31,11 +32,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -97,4 +98,9 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.0.0-rc01")
     //lottie animations
     //implementation("com.airbnb.android:lottie-compose:6.1.0")
+    
+    // Room components
+    ksp("androidx.room:room-compiler:2.5.0")
+    implementation("androidx.room:room-runtime:2.6.0")
+    implementation("androidx.room:room-ktx:2.6.0")
 }
