c2d55dafe70d38706ddc5b8841e3bcf18a812a2c
MrClassicT <tristan.cuvelier@student.hogent.be>
2024-01-07 01:35:25 +0100

    Update TODO list

--- app/src/main/java/com/android/dailycat/TODOS.kt
+++ app/src/main/java/com/android/dailycat/TODOS.kt
@@ -2,19 +2,12 @@ package com.android.dailycat
 
 // TodoList
 
+// TODO - Fix sync issues between adding/removing posts from favorites.
 
+// TODO - Check if offline, if so, go to favorites tab.
 
-// SRC: https://dev.to/arkilis/download-image-from-url-in-kotlin-4fc6
-// TODO - COIL for images? -> https://github.com/coil-kt/coil#jetpack-compose
-
-
-// TODO - Star to favorite disappears somehow?
-
-// TODO - Add functionality to favorite a post.
+// TODO - Check if connection is restored, then fetch new posts.
 
 // TODO - Write tests for fetching and favoriting?
 
-
-
 // TODO - add contentDescription for visual elements. -> If element is simply for visual decoration, set it to Null.
-
