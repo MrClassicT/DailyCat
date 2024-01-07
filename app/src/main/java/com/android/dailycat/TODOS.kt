b09330dd738923fa174fd9b6e167f4d6bc310771
MrClassicT <tristan.cuvelier@student.hogent.be>
2024-01-07 15:24:18 +0100

    updated todo

--- app/src/main/java/com/android/dailycat/TODOS.kt
+++ app/src/main/java/com/android/dailycat/TODOS.kt
@@ -2,11 +2,9 @@ package com.android.dailycat
 
 // TodoList
 
-
-
 // TODO - Write tests for fetching and favoriting?
 
 // TODO - Add adapt to landscape -> Change img/quote positioning.
 
-// TODO - add contentDescription for visual elements. -> If element is simply for visual decoration, set it to Null.
+// TODO - Documentation!
 
